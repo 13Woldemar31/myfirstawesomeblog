18c22cd4944e2a8a2df0a65d207917425b390860
Woldemar <73822511+Woldemar666@users.noreply.github.com>
2020-11-12 13:47:24 +0300

    Final project

--- blog/models.py
+++ blog/models.py
@@ -7,4 +7,9 @@ class Blog(models.Model):
 	blog_text = models.TextField()
 	blog_image = models.ImageField(upload_to='blog_images/')
 	
-	 
+	def get_summary(self):
+		return self.blog_text[:70]
+
+
+	def __str__(self):
+		return self.blog_title
